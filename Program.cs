40db1473772b2fab58bbad0518444b2c1e538f37
fabricio <fnmamao@gmail.com>
2020-05-15 16:12:34 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            Console.WriteLine("digite seu nome...");
+            nome = Console.ReadLine();
+            Console.WriteLine("digite seu sobrenome...");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine("seu nome completo: " + nome + sobrenome);
+            Console.WriteLine("nome de catalogo: " + sobrenome + nome);
+
+        }
+    }
+}
